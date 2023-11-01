0cb5b61e75f63619c7b024e8c7e4e72971d9cf4f
Hessam Mahdiabadi <67460597+iamHEssam@users.noreply.github.com>
2023-11-01 12:24:37 +0330

    Initial Commit

---
+++ MediaList/MediaListApp.swift
@@ -0,0 +1,17 @@
+//
+//  MediaListApp.swift
+//  MediaList
+//
+//  Created by Hessam Mahdiabadi on 11/1/23.
+//
+
+import SwiftUI
+
+@main
+struct MediaListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
